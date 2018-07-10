d7c60d1f96eb4f5c6b2e90373455ec50c8b69e7e
Wolfgang Goedel <wgoedel@gmail.com>
2018-07-10 08:28:57 +0200

    simple gql server

---
+++ packages/gql-server/db.js
@@ -0,0 +1,9 @@
+module.exports = {
+  books: [
+    { id: 1, title: "Die Welt von gestern", author_id: 1 },
+    { id: 2, title: "Die Schachnovelle", author_id: 1 },
+    { id: 3, title: "Der Prozess", author_id: 2 },
+    { id: 4, title: "Die Verwandlung", author_id: 2 }
+  ],
+  authors: [{ id: 1, name: "Stephan Zweig" }, { id: 2, name: "Franz Kafka" }]
+};
